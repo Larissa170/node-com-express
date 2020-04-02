0e7be87896e5fb0f7515773af13527db0cc416f9
Larissa170 <larissa.m.rodrigues1702@gmail.com>
2020-04-02 11:30:59 -0300

    git
    Signed-off-by: Larissa170 <larissa.m.rodrigues1702@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+
+const gitExtension =
+  vscode.extensions.getExtension < GitExtension > "vscode.git".exports;
+const git = gitExtension.getAPI(1);
+
 app.get("/", (req, res) => res.send("ola mundo express"));
 app.listen(port, () => console.log("Api rodando na porta 3000"));
